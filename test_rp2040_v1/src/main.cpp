c4e5951417a824039f741c0c2ad752b13b628893
mehdi bagherimofidi <mehdi.bagherimofidi@yahoo.com>
2021-10-11 14:13:40 +0330

    Add test of blinking

--- test_rp2040_v1/src/main.cpp
+++ test_rp2040_v1/src/main.cpp
@@ -1,10 +1,27 @@
 #include <Arduino.h>
 
+unsigned long pre_mili=0;
+bool state=FALSE;
 void setup() {
   // put your setup code here, to run once:
   Serial1.begin(115200);
+  Serial1.println("hello world");
+  pinMode(1,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if(millis()-pre_mili>1000)
+  {
+    if(state)
+    {
+      digitalWrite(1,LOW);
+    }
+    else
+    {
+      digitalWrite(1,HIGH);
+    }
+    per_mili=millis();
+  }
+
 }
